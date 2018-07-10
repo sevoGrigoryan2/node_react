7bc9b9aac1bfb1c744a14282a8f4582326e88ea6
Sevada <grigoryan.sevo1996@gmail.com>
2018-07-11 00:27:06 +0400

    add usermodel

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,31 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
+const User = require("../../models/users");
+const gravatar = require("gravatar");
+router.get("/", (req, res) => res.json({ msg: "Users Works" }));
 
-router.get("/",(req,res)=>res.json({msg:"Users Works"}));
+router.post('/regiter', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                res.status(400).json({ msg: "Email Already exist" })
+            } else {
+
+                const avatar = gravatar.url(req.body.url, {
+                    s: "200",//size
+                    r: "pg",//rating
+                    d: "mm"//default
+                });
+
+
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                })
+            }
+        })
+})
 
 module.exports = router;
