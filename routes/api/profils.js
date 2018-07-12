2fb555b5e6c808a0cbf6d7a84ac7dfec1651ef92
Sevada <grigoryan.sevo1996@gmail.com>
2018-07-13 01:36:38 +0400

    add Validation

--- routes/api/profils.js
+++ routes/api/profils.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/test",(req,res)=>res.json({msg:"Users Works"}));
+router.get("/test",(req,res)=>res.json({msg: "Users Works"}));
 
 module.exports = router;
