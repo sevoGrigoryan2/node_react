23ed899c4363c043320c9664df8d945e2a6c8742
Sevada <grigoryan.sevo1996@gmail.com>
2018-07-13 01:56:42 +0400

    change routs

---
+++ routes/api/usersCurrent.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
+// Load User model
+const User = require('../../models/users');
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+      });
+    }
+  );
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -12,12 +12,21 @@ app.get('/',(req,res)=>{
     res.send("Hello world");
 });
 
-const users = require("./routes/api/users");
+const usersLogin = require("./routes/api/usersLogin");
+const usersRegister = require("./routes/api/usersRegister");
+const usersCurrent = require("./routes/api/usersCurrent");
+
+
 const profils = require("./routes/api/profils");
 const posts = require("./routes/api/posts");
 
-app.use("/api/users",users);
+app.use("/api/users/login",usersLogin);
+app.use("/api/users/register",usersRegister);
+app.use("/api/users/current",usersCurrent);
+
+
 app.use("/api/profils",profils);
+
 app.use("/api/posts",posts);
 app.use(passport.initialize());
 
